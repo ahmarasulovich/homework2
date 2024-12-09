e47adf96336e24b3c34675d545f8216fe0b09912
abdusalamoff <abdusalamoff.1@mail.ru>
2024-12-09 19:50:57 +0300

    Дз переделал

--- module__3_5.py
+++ module__3_5.py
@@ -1,6 +1,9 @@
+
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
+    str_number = str_number.replace('0', '')
+
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
@@ -9,3 +12,5 @@ def get_multiplied_digits(number):
 
 result = get_multiplied_digits(40203)
 print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
