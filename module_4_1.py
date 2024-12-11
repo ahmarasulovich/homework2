39cdf8b2ee16de0c77ff49b86cbd15dec39fa771
abdusalamoff <abdusalamoff.1@mail.ru>
2024-12-11 21:28:38 +0300

    Дз

---
+++ module_4_1.py
@@ -0,0 +1,18 @@
+from fake_math import divide as dv
+from true_math import divide as di
+
+result1 = dv(69, 3)
+
+result2 = dv(3, 0)
+
+result3 = di(49, 7)
+
+result4 = di(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
