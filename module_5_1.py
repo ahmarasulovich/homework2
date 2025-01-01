df3e178f85e9f7a45984f94e9bf7f1fefa5f0dce
abdusalamoff <abdusalamoff.1@mail.ru>
2025-01-01 17:33:59 +0300

    Dz

--- module_5_1.py
+++ module_5_1.py
@@ -1,6 +1,3 @@
-from codecs import replace_errors
-
-
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
@@ -13,29 +10,10 @@ class House:
             for i in range(1, new_floor + 1):
                 print(f"Этаж {i}")
 
-    def __len__(self):
-         return self.number_of_floors
+h1 = House('ЖК Горский', 18)
 
-    def __str__(self):
-        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+h2 = House('Домик в деревне', 2)
 
+h1.go_to(5)
 
-h1 = House('ЖК Эльбрус', 10)
-
-h2 = House('ЖК Акация', 20)
-
-
-
-# __str__
-
-print(h1)
-
-print(h2)
-
-
-
-# __len__
-
-print(len(h1))
-
-print(len(h2))
+h2.go_to(10)
